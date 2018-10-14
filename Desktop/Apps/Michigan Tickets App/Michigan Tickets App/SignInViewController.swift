084c046de5019c1339cc9be5f6bfae51449405a7
Sahil Dogra <dogras@umich.edu>
2018-10-13 23:45:23 -0400

    Splash and sign-in

---
+++ Desktop/Apps/Michigan Tickets App/Michigan Tickets App/SignInViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SignInViewController.swift
+//  Michigan Tickets App
+//
+//  Created by Sahil Dogra on 10/13/18.
+//  Copyright © 2018 Sahil Dogra. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class SignInViewController: UIViewController, GIDSignInUIDelegate {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+        
+        // TODO(developer) Configure the sign-in button look/feel
+        // ...
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Desktop/Apps/Michigan Tickets App/Pods/Target Support Files/Pods-Michigan Tickets App/Pods-Michigan Tickets App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Michigan_Tickets_App : NSObject
+@end
+@implementation PodsDummy_Pods_Michigan_Tickets_App
+@end
